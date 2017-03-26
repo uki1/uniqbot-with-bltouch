49bc53d304895f727a5928b749efdee224299b49
uki1 <ykira599@gmail.com>
2017-03-26 18:03:53 +0500

    1
    1

--- Marlin-1.1.0-RC7/Marlin/Configuration.h
+++ Marlin-1.1.0-RC7/Marlin/Configuration.h
@@ -478,7 +478,7 @@
 //  (0,0)
 #define X_PROBE_OFFSET_FROM_EXTRUDER -27  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 2  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.2   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.4   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
