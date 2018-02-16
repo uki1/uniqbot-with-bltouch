d604ab5706f6891679bf3e034526ffc21c55fabc
uki1 <ykira599@gmail.com>
2018-02-16 19:06:32 +0500

    новая версия

--- Marlin-1.1.0-RC7 шнековый/Marlin/Configuration.h
+++ Marlin-1.1.0-RC7 шнековый/Marlin/Configuration.h
@@ -476,8 +476,8 @@
 //    |           |
 //    O-- FRONT --+
 //  (0,0)
-#define X_PROBE_OFFSET_FROM_EXTRUDER +27  // X offset: -left  +right  [of the nozzle]
-#define Y_PROBE_OFFSET_FROM_EXTRUDER 0  // Y offset: -front +behind [the nozzle]
+#define X_PROBE_OFFSET_FROM_EXTRUDER -57  // X offset: -left  +right  [of the nozzle]
+#define Y_PROBE_OFFSET_FROM_EXTRUDER -27  // Y offset: -front +behind [the nozzle]
 #define Z_PROBE_OFFSET_FROM_EXTRUDER -1.9   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
